bc7205c08e0f997d1a53804ce68a6ec15b3adb72
JEkittiMarion <95040061+JEkittiMarion@users.noreply.github.com>
2022-05-10 01:22:53 +0200

    Add files via upload
    step 05

---
+++ step5.py
@@ -0,0 +1,74 @@
+class Character():
+    def __init__(self,name,RPGClass):
+        self.name = name
+        self.RPGClass = RPGClass
+        self.life = 5
+        self.agility = 2
+        self.strength = 2
+        self.wit = 2
+
+    def attack(self):
+        self.sentence = "Rrrrrr....."
+        print (self.name, ':', self.sentence)
+
+    def unsheathes(self):
+        print ( self.name,': unsheates his weapon')
+
+    #class Movable(self):
+    def moveRight(self):
+        print (self.name,': moves right')
+    def moveLeft(self):
+        print (self.name,': moves left')
+    def moveBack(self):
+        print (self.name,': moves back')
+    def moveForward(self):
+        print (self.name,': moves forward')
+
+class Warrior (Character):
+    def __init__(self,name):
+        self.name = name
+        self.RPGClass = "Warrior"
+        self.life = 100
+        self.agility = 8
+        self.strength = 10
+        self.wit = 3
+
+    def attack(self,weapon):
+        if weapon == "hammer":
+            print (self.name, ': I ll crush you with my', weapon,'!')
+        elif weapon == "sword":
+            print (self.name, ': I ll crush you with my', weapon,'!')
+        else:
+            print (self.name," cant attack")
+        
+
+class Mage (Character):
+    def __init__(self,name):
+        self.name = name
+        self.RPGClass = "Mage"
+        self.life = 70
+        self.agility = 10
+        self.strength = 3
+        self.wit = 10
+
+    def attack(self,weapon):
+        if weapon == "magic":
+            print (self.name, ': Feel the power of my', weapon,'!')
+        elif weapon == "wand":
+            print (self.name, ': Feel the power of my', weapon,'!')
+        else:
+            print (self.name," cant attack")
+
+
+users = Character ("Jean-Luc","Character")
+users.attack()
+users.unsheathes()
+warrior = Warrior("Jean")
+warrior.unsheathes()
+warrior.attack("sword")
+warrior.moveRight()
+mage = Mage("Luc")
+mage.unsheathes()
+mage.attack("wan")
+mage.moveForward()
+
